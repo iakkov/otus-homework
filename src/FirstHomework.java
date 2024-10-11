2aa680f42dfefc9dd0ad1e655b5084409c1eb2ca
iakovlysenko <lysenko_iakov@yahoo.com>
2024-10-11 23:21:45 +0300

    Реализация метода greetings();

---
+++ src/FirstHomework.java
@@ -0,0 +1,9 @@
+public class FirstHomework {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello \nWorld \nfrom \nJava");
+    }
+}
