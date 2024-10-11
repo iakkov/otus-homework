faf0e07d44a71a613d570c95f800783743f4f9c3
iakovlysenko <lysenko_iakov@yahoo.com>
2024-10-11 23:24:29 +0300

    Реализация метода checkSign();

--- src/FirstHomework.java
+++ src/FirstHomework.java
@@ -6,4 +6,8 @@ public class FirstHomework {
     public static void greetings() {
         System.out.println("Hello \nWorld \nfrom \nJava");
     }
+    public static void checkSign(int a, int b, int c) {
+        if ((a + b + c) >= 0) System.out.println("Сумма положительная");
+        else System.out.println("Сумма отрицательная");
+    }
 }
