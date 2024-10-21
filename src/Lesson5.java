8c1a50ff936126525a753b0f9509878584a3a4de
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-10-21 06:34:17 +0300

    fixup! Метод с выводом строки n-количество раз

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -1,10 +1,22 @@
 public class Lesson5 {
     public static void main(String[] args) {
+        countString(5, "Кошка");
+        int[] a = {10, 20, 4, 5, 15, 3};
+        sumElements(a);
 
     }
-    public void countString(int a, String string) {
+    public static void countString(int a, String string) {
         for (int i = 0; i < a; i++) {
             System.out.println(string);
         }
+    }
+    public static void sumElements(int[] array) {
+        int sum = 0;
+        for (int num : array) {
+            if (num > 5) {
+                sum += num;
+            }
+        }
+        System.out.println("Сумма элементов больше 5: " + sum);
     }
 }
