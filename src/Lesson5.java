bab852f7151fa847937b1d01d13e4036410efb1c
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-10-21 06:31:15 +0300

    Метод с массивом и суммой элементов больше 5

---
+++ src/Lesson5.java
@@ -0,0 +1,10 @@
+public class Lesson5 {
+    public static void main(String[] args) {
+
+    }
+    public void countString(int a, String string) {
+        for (int i = 0; i < a; i++) {
+            System.out.println(string);
+        }
+    }
+}
