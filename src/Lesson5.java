2f33f4c142848564c501ea62652096357393c2e1
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-10-21 07:02:00 +0300

    Метод, вычисляющий какая половина массива больше

--- src/Lesson5.java
+++ src/Lesson5.java
@@ -3,7 +3,8 @@ public class Lesson5 {
         countString(5, "Кошка");
         int[] a = {10, 20, 4, 5, 15, 3};
         sumElements(a);
-        increaseElements(5, a);
+        //increaseElements(5, a);
+        sumHalfIsBigger(a);
     }
     public static void countString(int a, String string) {
         for (int i = 0; i < a; i++) {
@@ -27,6 +28,25 @@ public class Lesson5 {
     public static void increaseElements(int a, int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] += a;
+        }
+    }
+    public static void sumHalfIsBigger(int[] array) {
+        int firstHalfSum = 0;
+        int secondHalfSum = 0;
+        for (int i = 0; i < (array.length / 2); i++) {
+            firstHalfSum += array[i];
+        }
+        for (int j = array.length - 1; j >= (array.length / 2); j--) {
+            secondHalfSum += array[j];
+        }
+        if (firstHalfSum > secondHalfSum) {
+            System.out.println("Сумма элементов первой половины массива больше. Она равна " + firstHalfSum);
+        }
+        if (secondHalfSum > firstHalfSum) {
+            System.out.println("Сумма элементов второй половины массива больше. Она равна " + secondHalfSum);
+        }
+        if (secondHalfSum == firstHalfSum) {
+            System.out.println("Суммы элементов обеих половин равны");
         }
     }
 
