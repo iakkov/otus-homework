d9bdc16e2e685e82a91e639a8f0ab0bb27929b03
iakovlysenko <lysenko_iakov@yahoo.com>
2024-10-27 18:55:24 +0300

    Реализация sumOfPositiveElements

--- src/hello/world/pack/Lesson7.java
+++ src/hello/world/pack/Lesson7.java
@@ -1,4 +1,19 @@
 package hello.world.pack;
 
 public class Lesson7 {
+    public static void main(String[] args) {
+        int[][] array = {{2, 4, 6, -5, -10}, {5, -8, 0, 1, -7}};
+        System.out.println(sumOfPositiveElements(array));
+    }
+    public static int sumOfPositiveElements(int[][] array) {
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] > 0) {
+                    sum += array[i][j];
+                }
+            }
+        }
+        return sum;
+    }
 }
