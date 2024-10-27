6872ab7ed676c56bc132b6a48e837133b3655ba3
iakovlysenko <lysenko_iakov@yahoo.com>
2024-10-28 00:37:34 +0300

    Метод, который рисует обе диагонали

--- src/hello/world/pack/Lesson7.java
+++ src/hello/world/pack/Lesson7.java
@@ -3,9 +3,12 @@ package hello.world.pack;
 public class Lesson7 {
     public static void main(String[] args) {
         int[][] array = {{2, 4, 6, -5, -10}, {5, -8, 0, 1, -7}};
+        int[][] array1 = new int[5][5];
         System.out.println(sumOfPositiveElements(array));
 
-        printStarsSquare(5);
+        //printStarsSquare(5);
+        fillArray(array1);
+        printZeroDiagonal(array1);
     }
     public static int sumOfPositiveElements(int[][] array) {
         int sum = 0;
@@ -25,6 +28,25 @@ public class Lesson7 {
                 System.out.print("*");
             }
             System.out.println();
+        }
+    }
+    public static void printZeroDiagonal(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0, k = array[i].length - 1; j < array[i].length; j++, k--) {
+                if (i == j || i == k) {
+                    array[i][j] = 0;
+                    System.out.print(array[i][j]);
+                }
+                else System.out.print(array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    public static void fillArray(int[][] array) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = 1;
+            }
         }
     }
 }
