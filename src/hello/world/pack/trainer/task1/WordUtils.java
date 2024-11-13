0ca0244bca3d82a90cfb7a289f776ecb6a5cb80f
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-11-13 22:42:09 +0300

    Задание 1: WordUtils, задание

--- src/hello/world/pack/trainer/task1/WordUtils.java
+++ src/hello/world/pack/trainer/task1/WordUtils.java
@@ -6,4 +6,18 @@ package hello.world.pack.trainer.task1;
  * Например, для строки "test abc" необходимо получить строку "abc test".
  */
 public class WordUtils {
+    public static String reverseWords(String inputText) {
+        // TODO
+        return "";
+    }
+
+    private WordUtils() {
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ввод пустой строки - " + WordUtils.reverseWords(""));
+        System.out.println("Ввод одного слова - " + WordUtils.reverseWords("word"));
+        System.out.println("Ввод двух слов - " + WordUtils.reverseWords("два слова"));
+    }
 }
