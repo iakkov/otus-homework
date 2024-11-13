77e326acdc71be0bda55f5b85fe24bd410f9c7f3
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-11-13 22:47:21 +0300

    Задание 2: BracketUtil

---
+++ src/hello/world/pack/trainer/task2/BracketUtil.java
@@ -0,0 +1,24 @@
+package hello.world.pack.trainer.task2;
+
+/**
+ * Дана строка, состоящая из скобок, а также открывающий и закрывающий символ скобок.
+ * Необходимо реализовать метод, который проверяет что в строке у каждой открывающей скобки есть соответствующая ей закрывающая скобка.
+ * Строка в которой закрывающая скобка идёт впереди открывающей является некорректной.
+ * Например (()) - корректная строка, ()) - некорректная строка
+ */
+public class BracketUtil {
+    public static boolean isCorrectBrackets(String input, char bracketOpenSymbol, char bracketCloseSymbol) {
+        //TODO
+        return true;
+    }
+
+    private BracketUtil() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Ввод равного поличества открывающих и закрывающих скобок - "
+                + BracketUtil.isCorrectBrackets("{{}}", '{', '}'));
+        System.out.println("Ввод неравного поличества открывающих и закрывающих скобок - "
+                + BracketUtil.isCorrectBrackets("{{}}}", '{', '}'));
+    }
+}
