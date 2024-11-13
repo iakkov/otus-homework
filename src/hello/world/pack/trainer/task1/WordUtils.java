f885144600c552366e08adf58d77ffe7b13f16f0
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-11-13 22:45:12 +0300

    Задание 1: выполненное задание

--- src/hello/world/pack/trainer/task1/WordUtils.java
+++ src/hello/world/pack/trainer/task1/WordUtils.java
@@ -7,8 +7,18 @@ package hello.world.pack.trainer.task1;
  */
 public class WordUtils {
     public static String reverseWords(String inputText) {
-        // TODO
-        return "";
+        if (inputText == null || inputText.isEmpty()) {
+            return inputText;
+        }
+        String[] words = inputText.split(" ");
+        StringBuilder reversed = new StringBuilder();
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+        return reversed.toString();
     }
 
     private WordUtils() {
