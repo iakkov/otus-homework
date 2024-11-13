a7f880ad4c4e4f3cf14923ea986bdf37a1a2fdb8
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-11-13 22:49:54 +0300

    Задание 2: выполненное задание

--- src/hello/world/pack/trainer/task2/BracketUtil.java
+++ src/hello/world/pack/trainer/task2/BracketUtil.java
@@ -1,4 +1,5 @@
 package hello.world.pack.trainer.task2;
+import java.util.ArrayList;
 
 /**
  * Дана строка, состоящая из скобок, а также открывающий и закрывающий символ скобок.
@@ -7,9 +8,18 @@ package hello.world.pack.trainer.task2;
  * Например (()) - корректная строка, ()) - некорректная строка
  */
 public class BracketUtil {
-    public static boolean isCorrectBrackets(String input, char bracketOpenSymbol, char bracketCloseSymbol) {
-        //TODO
-        return true;
+    public static boolean isCorrectBrackets(String input,
+                                            char bracketOpenSymbol,
+                                            char bracketCloseSymbol) {
+        ArrayList<Character> chars = new ArrayList<Character>();
+        for (int i = 0; i < input.length(); i++) {
+            chars.add(input.charAt(i));
+        }
+        while (chars.contains(bracketOpenSymbol)) {
+            chars.remove(chars.indexOf(bracketOpenSymbol));
+            chars.remove(chars.indexOf(bracketCloseSymbol));
+        }
+        return chars.isEmpty();
     }
 
     private BracketUtil() {
@@ -20,5 +30,6 @@ public class BracketUtil {
                 + BracketUtil.isCorrectBrackets("{{}}", '{', '}'));
         System.out.println("Ввод неравного поличества открывающих и закрывающих скобок - "
                 + BracketUtil.isCorrectBrackets("{{}}}", '{', '}'));
+
     }
 }
