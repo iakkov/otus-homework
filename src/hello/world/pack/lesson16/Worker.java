5aaf39011d8dee17948a55f3636328e5a9a6ee2a
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-11-25 21:27:00 +0300

    Создан класс Сотрудник

---
+++ src/hello/world/pack/lesson16/Worker.java
@@ -0,0 +1,10 @@
+package hello.world.pack.lesson16;
+
+public class Worker {
+    String name;
+    int age;
+    public Worker(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
