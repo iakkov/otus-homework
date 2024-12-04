2eb958e2e5ddca887126c3853f6ec5ccb3841367
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-12-04 11:49:01 +0300

    Создан класс PersonDataBase

---
+++ src/hello/world/pack/lesson19/PersonDataBase.java
@@ -0,0 +1,13 @@
+package hello.world.pack.lesson19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonDataBase {
+    List<Person> persons;
+
+    public PersonDataBase(ArrayList<Person> persons) {
+        this.persons = persons;
+    }
+
+}
