e20b23f223a74b321d11562224f6310def4eeb1e
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-12-08 21:58:46 +0300

    * вроде как понял как работает сортировка quickSort

--- src/hello/world/pack/lesson19/sort/Main.java
+++ src/hello/world/pack/lesson19/sort/Main.java
@@ -3,11 +3,13 @@ package hello.world.pack.lesson19.sort;
 import java.util.Arrays;
 
 import static hello.world.pack.lesson19.sort.Sort.bubbleSort;
+import static hello.world.pack.lesson19.sort.Sort.quickSort;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {22, 58, 12, 45, 94, 9, 3, 24, 35};
-        bubbleSort(array);
+        int[] array = {22, 58, 12, 45, 94, 9, 3, 24, 35, 71, 923, 783, 19, 123, 98};
+        //bubbleSort(array);
+        quickSort(array, 0, array.length - 1);
         System.out.println(Arrays.toString(array));
     }
 }
