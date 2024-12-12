abfb0697ce8c5a3d89984ca417bdc6e57097c6b2
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-12-12 23:08:21 +0300

    Вывод на экран списка файлов в директории

--- src/hello/world/pack/lesson20/Main.java
+++ src/hello/world/pack/lesson20/Main.java
@@ -1,7 +1,21 @@
 package hello.world.pack.lesson20;
+import java.io.File;
+import java.nio.file.Paths;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        File file = new File("src/hello/world/pack/lesson20/files");
+        File[] arrayFile = file.listFiles();
+        for (int i = 0; i < file.listFiles().length; i++) {
+            String string = String.valueOf(arrayFile[i]);
+            StringBuilder stringBuilder = new StringBuilder(string);
+            int n = string.lastIndexOf("\\");
+            String newString = stringBuilder.substring(n + 1, string.length());
+            System.out.println(newString);
+        }
+        
 
     }
+
 }
