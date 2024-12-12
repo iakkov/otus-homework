0c066294f0e916ea80add434fb41930c7e2cff44
Iakov Lysenko <lysenko_iakov@yahoo.com>
2024-12-12 23:19:58 +0300

    Отдельный метод fileList(File[] array) для вывода на экран списка файлов

--- src/hello/world/pack/lesson20/Main.java
+++ src/hello/world/pack/lesson20/Main.java
@@ -4,18 +4,20 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class Main {
+    private static final File FILE = new File("src/hello/world/pack/lesson20/files");
+    private static File[] arrayFile = FILE.listFiles();
+
     public static void main(String[] args) {
-        File file = new File("src/hello/world/pack/lesson20/files");
-        File[] arrayFile = file.listFiles();
-        for (int i = 0; i < file.listFiles().length; i++) {
-            String string = String.valueOf(arrayFile[i]);
+        fileList(arrayFile);
+    }
+    public static void fileList(File[] array) {
+        for (int i = 0; i < array.length; i++) {
+            String string = String.valueOf(array[i]);
             StringBuilder stringBuilder = new StringBuilder(string);
             int n = string.lastIndexOf("\\");
             String newString = stringBuilder.substring(n + 1, string.length());
             System.out.println(newString);
         }
-        
-
     }
 
 }
