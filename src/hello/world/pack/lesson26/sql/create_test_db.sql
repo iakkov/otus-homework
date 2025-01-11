85d4bcb1e8bdf5f19efe94aeb25bdf9ed8a5c781
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-11 13:54:18 +0300

    Добавление теста, вопросов, ответов

--- src/hello/world/pack/lesson26/sql/create_test_db.sql
+++ src/hello/world/pack/lesson26/sql/create_test_db.sql
@@ -17,3 +17,18 @@ CREATE TABLE answers (
                          answer_text TEXT NOT NULL,
                          is_correct BOOLEAN NOT NULL
 );
+
+INSERT INTO tests (test_name) VALUES ('Тест по программированию');
+
+INSERT INTO questions (test_id, question_text) VALUES
+                                                   (1, 'Какой язык программирования используется для разработки Android?'),
+                                                   (1, 'Что такое ООП?');
+
+INSERT INTO answers (question_id, answer_text, is_correct) VALUES
+                                                               (1, 'Java', TRUE),
+                                                               (1, 'Python', FALSE),
+                                                               (1, 'C++', FALSE),
+                                                               (1, 'JavaScript', FALSE),
+                                                               (2, 'Объектно-ориентированное программирование', TRUE),
+                                                               (2, 'Операционная система', FALSE),
+                                                               (2, 'Объектно-оптимизированное программирование', FALSE);
