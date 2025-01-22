86bde571916cc4b5518005c30106b33b9059d1fe
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-22 12:34:11 +0300

    Использование буфера

--- src/hello/world/pack/lesson29/classWork/Main.java
+++ src/hello/world/pack/lesson29/classWork/Main.java
@@ -8,9 +8,12 @@ import java.nio.charset.StandardCharsets;
 public class Main {
     public static void main(String[] args) {
         try (FileInputStream fileInputStream = new FileInputStream("src/hello/world/pack/lesson29/classWork/1.txt")) {
+            byte[] buffer = new byte[1024];
+            int length;
             int byteRead;
-            while((byteRead = fileInputStream.read()) != -1) {
-                System.out.print((char) byteRead);
+            while((length = fileInputStream.read(buffer)) != -1) {
+                String data = new String(buffer, 0, length);
+                System.out.print(data);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
