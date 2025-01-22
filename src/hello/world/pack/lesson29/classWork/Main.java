eaf29ef5b9f44a9af24ed0a48d3d2a130fed06eb
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-22 12:27:25 +0300

    Реализация через ASCII

--- src/hello/world/pack/lesson29/classWork/Main.java
+++ src/hello/world/pack/lesson29/classWork/Main.java
@@ -6,10 +6,10 @@ import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try (FileReader fileReader = new FileReader("src\\hello\\world\\pack\\lesson29\\classWork\\1.txt", StandardCharsets.UTF_8)) {
+        try (FileReader fileReader = new FileReader("src\\hello\\world\\pack\\lesson29\\classWork\\1.txt", StandardCharsets.US_ASCII)) {
             int byteRead;
             while((byteRead = fileReader.read()) != -1) {
-                System.out.println((char) byteRead);
+                System.out.print((char) byteRead);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
