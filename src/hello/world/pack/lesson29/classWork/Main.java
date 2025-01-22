2d1152bc3f91e7b3599a1c0c0009478cf7588ec3
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-01-22 12:30:56 +0300

    Использование FileInputStream

--- src/hello/world/pack/lesson29/classWork/Main.java
+++ src/hello/world/pack/lesson29/classWork/Main.java
@@ -1,14 +1,15 @@
 package hello.world.pack.lesson29.classWork;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
-        try (FileReader fileReader = new FileReader("src\\hello\\world\\pack\\lesson29\\classWork\\1.txt", StandardCharsets.US_ASCII)) {
+        try (FileInputStream fileInputStream = new FileInputStream("src/hello/world/pack/lesson29/classWork/1.txt")) {
             int byteRead;
-            while((byteRead = fileReader.read()) != -1) {
+            while((byteRead = fileInputStream.read()) != -1) {
                 System.out.print((char) byteRead);
             }
         } catch (IOException e) {
