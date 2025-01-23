4215730a500f3ff1850b6d4db2975fb5d4aa9e08
iakovlysenko <lysenko_iakov@yahoo.com>
2025-01-23 22:16:59 +0300

    Использование OutputStream

--- src/hello/world/pack/lesson29/classWork/Main.java
+++ src/hello/world/pack/lesson29/classWork/Main.java
@@ -1,19 +1,25 @@
 package hello.world.pack.lesson29.classWork;
 
 import java.io.FileInputStream;
-import java.io.FileReader;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 public class Main {
     public static void main(String[] args) {
         try (FileInputStream fileInputStream = new FileInputStream("src/hello/world/pack/lesson29/classWork/1.txt")) {
             byte[] buffer = new byte[1024];
             int length;
-            int byteRead;
             while((length = fileInputStream.read(buffer)) != -1) {
                 String data = new String(buffer, 0, length);
                 System.out.print(data);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try (FileOutputStream fileOutputStream = new FileOutputStream("src/hello/world/pack/lesson29/classWork/2.txt")) {
+            for (char ch : "Hello World".toCharArray()) {
+                fileOutputStream.write(ch);
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
