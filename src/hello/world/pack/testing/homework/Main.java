d7f144809c496526f97e102ed185cd44386d4189
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-02-09 17:56:49 +0300

    Реализация метода isCorrectArray(int[] array)

--- src/hello/world/pack/testing/homework/Main.java
+++ src/hello/world/pack/testing/homework/Main.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println(Arrays.toString(arrayFromTheLastOne(new int[]{2,2,2,2,2})));
+        System.out.println(Arrays.toString(arrayFromTheLastOne(new int[]{2,1,2,2,2})));
+        System.out.println(isCorrectArray(new int[] {2,2,2,2,2,2}));
     }
 
     public static int[] arrayFromTheLastOne(int[] array) {
@@ -22,5 +23,25 @@ public class Main {
         }
         int[] newArray = Arrays.copyOfRange(array, index + 1, array.length);
         return newArray;
+    }
+    public static boolean isCorrectArray(int[] array) {
+        boolean isOneExisting = false;
+        boolean isTwoExisting = false;
+        for (Integer i : array) {
+            if (i == 1) {
+                isOneExisting = true;
+                continue;
+            }
+            if (i == 2) {
+                isTwoExisting = true;
+                continue;
+            }
+            if (i != 1 || i != 2) {
+                return false;
+            }
+        }
+        if (isOneExisting && isTwoExisting) {
+            return true;
+        } else return false;
     }
 }
