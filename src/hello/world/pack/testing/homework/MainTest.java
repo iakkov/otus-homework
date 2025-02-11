9851481198f29a31f765634dcc2d3b5a790971a3
Iakov Lysenko <lysenko_iakov@yahoo.com>
2025-02-11 07:12:45 +0300

    Исправления

--- src/hello/world/pack/testing/homework/MainTest.java
+++ src/hello/world/pack/testing/homework/MainTest.java
@@ -12,21 +12,34 @@ class MainTest {
     @DisplayName("Метод 1")
     void arrayFromTheLastOne() {
         int[] expectedArray = new int[] {2, 2};
+
         int[] actualArray = Main.arrayFromTheLastOne(new int[] {1, 2, 1, 2, 2});
+
         Assertions.assertArrayEquals(expectedArray, actualArray);
     }
 
     @Test
-    @DisplayName("Метод 2")
-    void isCorrectArray() {
+    @DisplayName("Метод 2 c true")
+    void isCorrectArrayTrue() {
         int[] array1 = new int[] {1, 2};
-        int[] array2 = new int[] {1, 1};
-        int[] array3 = new int[] {1, 3};
         int[] array4 = new int[] {1, 2, 2, 1};
 
-        Assertions.assertTrue(Main.isCorrectArray(array1));
-        Assertions.assertFalse(Main.isCorrectArray(array2));
-        Assertions.assertFalse(Main.isCorrectArray(array3));
-        Assertions.assertTrue(Main.isCorrectArray(array4));
+        boolean trueArray1 = Main.isCorrectArray(array1);
+        boolean trueArray2 = Main.isCorrectArray(array4);
+
+        Assertions.assertTrue(trueArray1);
+        Assertions.assertTrue(trueArray2);
+    }
+    @Test
+    @DisplayName("Метод 2 с false")
+    void isCorrectArrayFalse() {
+        int[] array2 = new int[] {1, 1};
+        int[] array3 = new int[] {1, 3};
+
+        boolean falseArray1 = Main.isCorrectArray(array2);
+        boolean falseArray2 = Main.isCorrectArray(array3);
+
+        Assertions.assertFalse(falseArray1);
+        Assertions.assertFalse(falseArray2);
     }
 }
